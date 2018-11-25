df2bd04df046d63f015b2054d90a8607519a3ff1
Kobi <kobikamalendran@outlook.com>
2018-11-25 13:54:41 +0000

    Add CRUD actions to Cars

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,6 +1,10 @@
 class CarsController < ApplicationController
+	before_action :find_car, only: [:show, :edit, :update, :destroy]
 	def index
 	  @cars = Car.all
+	end
+
+	def show
 	end
 
 	def new
@@ -16,9 +20,29 @@ class CarsController < ApplicationController
 	  end
 	end
 
+	def edit
+	end
+
+	def update
+	  if @car.update(car_params)
+	  	redirect_to car_path(@car)
+	  else
+	  	render 'edit'
+	  end
+	end
+
+	def destroy
+	  @car.destroy
+	  redirect_to root_path
+	end
+
 	private
 
 		def car_params
 		  params.require(:car).permit(:make, :model, :year, :contact)
+		end
+
+		def find_car
+		  @car = Car.find(params[:id])
 		end
 end
