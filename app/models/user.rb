7b70ba6be9a7271e4178e8ca24a50be1d383cd02
Kobi <kobikamalendran@outlook.com>
2018-11-25 17:47:37 +0000

    Added navbar to homepage

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_many :reviews
-  validates :email, presence:true
-  validates :password, presence:true, length: {minimum:8}
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
