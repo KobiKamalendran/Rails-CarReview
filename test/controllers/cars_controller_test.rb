39c9151a59df3abe270509e12a270c786d080316
Kobi <kobikamalendran@outlook.com>
2018-12-09 16:55:27 +0000

    Added Contact Us to navbar

--- test/controllers/cars_controller_test.rb
+++ test/controllers/cars_controller_test.rb
@@ -17,7 +17,34 @@ class CarsControllerTest < ActionDispatch::IntegrationTest
 		assert_response :success
 	end
 
+	test "should get contact" do
+		get contact_path
 
+		assert_response :success
+		assert_select 'h1', 'Contact Us'
+
+	
+	end
+
+	test "should post request contact but no email" do
+		post :request_contact
+
+		assert_response :redirect
+		assert_not_empty flash[:alert]
+		assert_nil flash[:notice]
+
+	end
+
+
+	test "should post request contact" do
+		post :request_contact,
+			name: "Matthew", email: "matthew@me.com",
+			message: "Interested"
+
+		assert_response :redirect
+		assert_nil flash[:alert]
+		assert_not_empty flash[:notice]
+	end
 
 
 
