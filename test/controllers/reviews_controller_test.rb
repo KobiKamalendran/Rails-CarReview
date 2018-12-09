38fdf8c099032b57b72acabc2955d3f2b45b4b85
Kobi <kobikamalendran@outlook.com>
2018-12-09 21:58:08 +0000

    Added reviews controller tests

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class ReviewsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "get new review page" do
+  	@car = cars(:one)
+  	get new_car_review_path(@car)
+  	assert :response
+  end
+
+  test "edit review" do 
+  	@car = cars(:one)
+  	@review = reviews(:one)
+  	get edit_car_review_path(@car,@review)
+  	assert :response
+  end
+
 end
