acb96d57f4d7eb056939b86db4c4d14edcd73e43
Kobi <kobikamalendran@outlook.com>
2018-12-10 17:06:36 +0000

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+types = Type.create([{ name: 'Saloon' }, { name: 'Hatchback' }, { name: 'SUV' }, { name: 'MPV' }, { name: 'Coupe' }])
